a924179943269fe8ecaa2eb5e42d62db01e758a0
AlexIonescu28 <alexdrionescu2805@gmail.com>
2026-03-22 20:59:26 +0200

    Enemy patrol

---
+++ Assets/Scripts/PatrolScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolScript : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    public Rigidbody2D enemyRigidBody;
+    private Transform currentPosition;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+        currentPosition = pointB.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPosition.position - transform.position;
+        if(currentPosition==pointB.transform)
+        {
+            enemyRigidBody.velocity = new Vector2(speed,0);
+        }
+        else
+        {
+            enemyRigidBody.velocity = new Vector2(-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position,currentPosition.position) < 0.5f && currentPosition==pointB.transform)
+        {
+            currentPosition = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPosition.position) < 0.5f && currentPosition == pointA.transform)
+        {
+            currentPosition = pointB.transform;
+        }
+    }
+}
